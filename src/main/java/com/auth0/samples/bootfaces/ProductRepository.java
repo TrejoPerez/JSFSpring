3cc4fa460d60fab0eef1611f5b60c7beb62b051d
TrejoPerez <trejo85@live.com.mx>
2017-09-29 00:26:46 -0500

    Plantilla basica de spring con JSF y prime faces

---
+++ src/main/java/com/auth0/samples/bootfaces/ProductRepository.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.auth0.samples.bootfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author joseatp
+ */
+public interface ProductRepository extends JpaRepository<Product, Long> {    
+}
